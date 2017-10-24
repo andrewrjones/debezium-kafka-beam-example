6e4fabde79a3527b3ac8b7faec84a0f7f0375549
Andrew Jones <andrew@andrew-jones.com>
2017-10-24 15:14:44 +0100

    Improved solution for Kafka consumer
    Based off suggestions from the mailing list.

--- src/main/java/com/andrewjones/KafkaAvroConsumerExample.java
+++ src/main/java/com/andrewjones/KafkaAvroConsumerExample.java
@@ -2,6 +2,7 @@ package com.andrewjones;
 
 import com.google.common.collect.ImmutableMap;
 import dbserver1.inventory.customers.Envelope;
+import dbserver1.inventory.customers.Value;
 import io.confluent.kafka.serializers.KafkaAvroDeserializer;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.coders.AvroCoder;
@@ -11,6 +12,8 @@ import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.*;
 import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PBegin;
+import org.apache.beam.sdk.values.PCollection;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
 public class KafkaAvroConsumerExample {
@@ -19,26 +22,11 @@ public class KafkaAvroConsumerExample {
         PipelineOptions options = PipelineOptionsFactory.create();
         Pipeline p = Pipeline.create(options);
 
-        p.apply(KafkaIO.<String, Object>read()
+        PTransform<PBegin, PCollection<KV<String, Envelope>>> kafka = KafkaIO.<String, Envelope>read()
                 .withBootstrapServers("kafka:9092")
                 .withTopic("dbserver1.inventory.customers")
                 .withKeyDeserializer(StringDeserializer.class)
-                /*
-                 * Using just the KafkaAvroDeserializer fails with:
-                 * Caused by: java.lang.RuntimeException: Unable to automatically infer a Coder for the Kafka
-                 * Deserializer class io.confluent.kafka.serializers.KafkaAvroDeserializer: no coder registered for
-                 * type class java.lang.Object
-                 */
-                .withValueDeserializer(KafkaAvroDeserializer.class)
-
-                /*
-                 * Using this and changing to KafkaIO.<String, Envelope>read() does not compile with:
-                 * Compilation failure
-                 * [ERROR] /usr/src/kafka/src/main/java/com/andrewjones/KafkaAvroConsumerExample.java:[36,58]
-                 * incompatible types: java.lang.Class<io.confluent.kafka.serializers.KafkaAvroDeserializer> cannot be
-                 * converted to java.lang.Class<? extends org.apache.kafka.common.serialization.Deserializer<dbserver1.inventory.customers.Envelope>>
-                 */
-//                .withValueDeserializerAndCoder(KafkaAvroDeserializer.class, AvroCoder.of(Envelope.class))
+                .withValueDeserializerAndCoder((Class)KafkaAvroDeserializer.class, AvroCoder.of(Envelope.class))
 
                 .updateConsumerProperties(ImmutableMap.of("auto.offset.reset", (Object)"earliest"))
                 .updateConsumerProperties(ImmutableMap.of("schema.registry.url", (Object)"http://registry:8081"))
@@ -49,15 +37,15 @@ public class KafkaAvroConsumerExample {
                 // In reality, we would likely be writing to a data source that supports unbounded data, such as BigQuery.
                 .withMaxNumRecords(2)
 
-                .withoutMetadata() // PCollection<KV<Long, String>>
-        )
-                .apply(Values.<Object>create())
-                .apply("ExtractWords", ParDo.of(new DoFn<Object, String>() {
+                .withoutMetadata();
+
+        p.apply(kafka)
+                .apply(Values.<Envelope>create())
+                .apply("ExtractWords", ParDo.of(new DoFn<Envelope, String>() {
                     @ProcessElement
                     public void processElement(ProcessContext c) {
-                        System.out.println(c.element());
-//                        System.out.println(c.element().getAfter().getEmail());
-                        c.output("hi");
+                        Value customer = c.element().getAfter();
+                        c.output(customer.getEmail().toString());
                     }
                 }))
                 .apply(Count.<String>perElement())
